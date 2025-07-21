3ac4c68a11799b6f790b0c9ff8f931619c0f1504
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2025-07-22 03:21:04 +0530

    Time: 36 ms (99.53%), Space: 22.2 MB (20.15%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+
+        # The idea is to have the parent of every node all the way to the top
+        # then using the target nodes, store all the parents we see for the first
+        # and use the first parent of the other nodes
+        parent_map = {}
+        def parent_mapper(node, parent):
+            if not node:
+                return
+            parent_map[node] = parent
+            parent_mapper(node.left, node)
+            parent_mapper(node.right, node)
+        parent_mapper(root, None)
+
+        seen = set()
+        cur = q
+        while cur:
+            seen.add(cur)
+            cur = parent_map[cur]
+
+        cur = p
+        while cur and cur not in seen:
+            cur = parent_map[cur]
+
+        return cur
