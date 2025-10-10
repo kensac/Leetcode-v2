c066deaded8a5987a74a5db8a2236e98adc52bcc
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2025-10-10 06:55:59 -0400

    Time: 0 ms (100%), Space: 17.7 MB (53.85%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -2,19 +2,18 @@ class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         if not nums:
             return []
-        
+
         start = cur = 0
         res = []
         while cur < len(nums):
             if nums[cur] + 1 in nums:
                 cur += 1
                 continue
-            elif start == cur:
+            if start == cur:
                 res.append(f"{nums[cur]}")
-                cur += 1
-                start = cur
             else:
                 res.append(f"{nums[start]}->{nums[cur]}")
-                cur += 1
-                start = cur
+
+            cur += 1
+            start = cur
         return res
