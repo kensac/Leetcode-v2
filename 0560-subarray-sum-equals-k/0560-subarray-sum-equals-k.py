967f17cf6a3049bf9f29bdfb324e74533b9e9c25
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2025-11-04 14:09:33 -0500

    Time: 29 ms (68.81%), Space: 20.3 MB (71.46%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -5,10 +5,14 @@ class Solution:
         sum_map = {}
         sum_map[0] = 1
 
+        # This is such a nice idea but we store the sum we encounter and the number of times we have seen it before
+        # the idea is when we see cur_sum - k in the map everything between cur and that val equal k
+        # we increment the count by the count in sum_map because that is how. many instances of the exact sum we have seen already before we got here
+
         for num in nums:
             cur_sum += num
             if cur_sum - k in sum_map:
                 count += sum_map[cur_sum - k]
             sum_map[cur_sum] = sum_map.get(cur_sum, 0) + 1
-        
+
         return count
