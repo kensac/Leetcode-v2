41c752a1cd2e5fd6e18dfe498b70673efe8abf1a
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2025-11-05 23:20:55 -0500

    Time: 3 ms (22.51%), Space: 17.7 MB (64.98%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -10,5 +10,4 @@ class Solution:
                 top = grid[i - 1][j] if i > 0 else 0
                 left = grid[i][j - 1] if j > 0 else 0
                 grid[i][j] = top + left
-        #print(grid)
         return grid[m - 1][n - 1]
