60b52c36b4230d12218c6a5491cb396fa39ea6ab
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2026-01-02 10:27:05 -0500

    Time: 0 ms (100%), Space: 17.2 MB (92.42%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,28 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def robber(nums):
-            current = 0
-            if not nums:
-                return 0
-            if len(nums) == 1:
-                return nums[0]
-            N = len(nums)
-
-            rob_next_plus_one = 0
-            rob_next = nums[N - 1]
-
-            for i in range(N - 2, -1, -1):
-                current = max(nums[i] + rob_next_plus_one, rob_next)
-
-                rob_next_plus_one = rob_next
-                rob_next = current
-
-            return current
-
-        if not nums:
+        if len(nums) == 0:
             return 0
-
         if len(nums) == 1:
             return nums[0]
 
-        return max(robber(nums[1:]), robber(nums[:-1]))
+        def robber(houses):
+            prev_one = 0
+            prev_two = 0
+            for val in houses:
+                prev_one, prev_two = max(prev_one, prev_two + val), prev_one
+            return prev_one
+
+        return max(robber(nums[1:].copy()), robber(nums[:-1]))
