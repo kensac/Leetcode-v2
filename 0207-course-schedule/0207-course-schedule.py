748591f16df8d357f0bf104c2592dc265194c9dc
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2026-01-04 16:27:23 -0500

    Time: 3 ms (86.86%), Space: 18.3 MB (97.46%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,20 +1,19 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # Model as graph
-        graph = defaultdict(set)
+        graph = defaultdict(list)
         indegree = [0 for _ in range(numCourses)]
-        for prev, cur in prerequisites:
-            graph[prev].add(cur)
-            indegree[cur] += 1
-
-
-        queue = deque([idx for idx, val in enumerate(indegree) if val == 0])
+        for course, requirement in prerequisites:
+            graph[requirement].append(course)
+            indegree[course] += 1
+        
+        queue = Deque([i for i in range(numCourses) if indegree[i] == 0])
 
         while queue:
-            cur = queue.popleft()
-            for neighbour in graph[cur]:
-                indegree[neighbour] -= 1
-                if indegree[neighbour] == 0:
-                    queue.append(neighbour)
-
-        return all([True if val==0 else False for val in indegree])
+            cur_course = queue.popleft()
+            
+            for course in graph[cur_course]:
+                indegree[course] -= 1
+                if indegree[course] == 0:
+                    queue.append(course)
+        
+        return all(i == 0 for i in indegree)
