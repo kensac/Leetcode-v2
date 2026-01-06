64089386e0d4dbf6451d9d7a83a3871159c52824
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2026-01-05 20:21:03 -0500

    Time: 3 ms (47.39%), Space: 18.6 MB (86.75%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,26 +1,27 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if len(edges) != n - 1:
+        if len(edges) != (n -1):
             return False
-
+            
         parent = [i for i in range(n)]
 
         def find(node):
-            while node != parent[node]:
-                node = parent[node]
-            return node
-
+            if parent[node] != node:
+                parent[node] = find(parent[node])
+            return parent[node]
+        
         def union(node1, node2):
-            p1 = find(node1)
-            p2 = find(node2)
-
-            if p1 == p2:
+            node1 = find(node1)
+            node2 = find(node2)
+            if node1 == node2:
                 return False
-            parent[p1] = p2
+            
+            parent[node1] = node2
+
             return True
 
-        for node1, node2 in edges:
-            if not union(node1, node2):
+        for start, end in edges:
+            if not union(start, end):
                 return False
 
-        return True
+        return True            
