89c5117d27ee5aa184258779ce5e7ec36c7b2038
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2026-01-06 02:43:29 -0500

    Time: 76 ms (97.63%), Space: 19.9 MB (16.47%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -8,12 +8,13 @@ class Solution:
 
         for r in range(len(s)):
             count[s[r]] += 1
+            # We don't care about recomputing this every cycle because we already have a maxf value where this works, if we find a new longer sequence we can deal with it there
             max_f = max(max_f, count[s[r]])
 
             while (r - l + 1) - max_f > k:
                 count[s[l]] -= 1
                 l += 1
-            
+
             res = max(res, (r - l + 1))
-        
+
         return res
