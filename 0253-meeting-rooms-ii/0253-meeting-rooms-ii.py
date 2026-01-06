3d1be9876606afb98e32a51bfdb450c02806edf8
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2026-01-05 19:23:20 -0500

    Time: 8 ms (16.52%), Space: 20 MB (41.85%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -5,15 +5,12 @@ class Solution:
         for start, end in intervals:
             events.append((start, 1))
             events.append((end, -1))
-
-        available = 0
+        
         events.sort()
+        max_rooms = 0
+        cur = 0
         for time, event in events:
-            if event == 1:
-                if available == 0:
-                    available += 1
-                if available != 0:
-                    available -= 1
-            elif event == -1:
-                available += 1
-        return available
+            cur += event
+            max_rooms = max(max_rooms, cur)
+        
+        return max_rooms
