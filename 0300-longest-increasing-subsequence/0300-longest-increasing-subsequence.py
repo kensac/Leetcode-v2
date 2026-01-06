3ddc093f3de34316720657a535e4a80940012f52
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2026-01-05 19:56:11 -0500

    Time: 3 ms (95.66%), Space: 17.4 MB (96.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,12 +1,15 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        # bisect left gives us insert position
+        # use natural sorting property of bisect function
         subsequence = []
 
         for num in nums:
             i = bisect_left(subsequence, num)
+
             if i == len(subsequence):
                 subsequence.append(num)
             else:
                 subsequence[i] = num
-
+            
         return len(subsequence)
