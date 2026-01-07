3fb9db536c0bfb395a2aee200a8f3a79e2dc5fb5
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2026-01-06 19:51:40 -0500

    Time: 262 ms (5.1%), Space: 20.7 MB (13.65%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def can_eat(piles, k, h):
+            hours = 0
+            for pile in piles:
+                hours += (pile // k) + (1 if pile % k else 0)
+            return hours <= h
+
+        left = 1
+        right = sum(piles)
+
+        """
+              L M    R
+        F F F F T T T
+        0 1 2 3 4 5 6
+        """
+        while left < right:
+            mid = (left + right) // 2
+            if can_eat(piles, mid, h):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
