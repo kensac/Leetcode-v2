faa6ff09b0e5813802436cb3807af13fb8c35b3d
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2026-06-23 10:38:20 -0400

    Time: 177 ms (39.28%), Space: 20.7 MB (41.82%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,24 +1,20 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def can_eat(piles, k, h):
+        def can_eat(speed):
             hours = 0
             for pile in piles:
-                hours += (pile // k) + (1 if pile % k else 0)
+                hours += (pile // speed) + (1 if (pile % speed) else 0)
+            
             return hours <= h
-
+        
         left = 1
         right = max(piles)
 
-        """
-              L M    R
-        F F F F T T T
-        0 1 2 3 4 5 6
-        """
         while left < right:
             mid = (left + right) // 2
-            if can_eat(piles, mid, h):
+            if can_eat(mid):
                 right = mid
             else:
                 left = mid + 1
-
-        return left
+        
+        return right
