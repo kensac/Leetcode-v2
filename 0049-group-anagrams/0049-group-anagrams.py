4371347ef067dadb64c8cc2396ca4bddbdd86ebd
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2026-07-21 15:16:55 -0400

    Time: 19 ms (19.95%), Space: 24 MB (9.17%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        """
-        How would you solve with unicode, sort the anagram map that costs more time but would support any amount or types of chars
-        """
-        anagram_map = defaultdict(list)
+        groups = defaultdict(list)
+
         for word in strs:
-            counter = [0 for _ in range(26)]
+            counter = [0] * 26
             for char in word:
                 counter[ord(char) - ord('a')] += 1
-            anagram_map[tuple(counter)].append(word)
+            groups[tuple(counter)].append(word)
         
-        return list(anagram_map.values())
+        return list(groups.values())
